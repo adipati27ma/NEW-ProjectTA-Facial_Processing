21e5af339854701aa8937b055af220a6c161d983
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-21 06:25:52 +0700

    initial commit

---
+++ video_facial_landmarks.py
@@ -0,0 +1,9 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils import face_utils
+import datetime
+import argparse
+import imutils
+import time
+import dlib
+import cv2
